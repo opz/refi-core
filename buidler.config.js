aa611ee3d1da7fa13ab9c8f79bc220a7ce7d1915
opz <will.shahda@gmail.com>
2020-05-26 17:25:50 -0400

    Create network config for Kovan

--- buidler.config.js
+++ buidler.config.js
@@ -1,6 +1,29 @@
 require("dotenv").config();
 
+function getEnv(env) {
+  let value = process.env[env];
+
+  if (typeof value == "undefined") {
+    value = "";
+    console.error(`WARNING: ${env} environment variable has not been set`);
+  }
+
+  return value;
+}
+
+const kovanEndpoint = getEnv("KOVAN_ENDPOINT");
+const kovanMnemonic = getEnv("KOVAN_MNEMONIC");
+
 module.exports = {
+  networks: {
+    kovan: {
+      url: kovanEndpoint,
+      chainId: 42,
+      accounts: {
+        mnemonic: kovanMnemonic
+      }
+    }
+  },
   solc: {
     version: "0.6.8",
     optimizer: {
