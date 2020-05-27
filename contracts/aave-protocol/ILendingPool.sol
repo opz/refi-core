896410ab0a83838f64a39231f02242a9141aff89
opz <will.shahda@gmail.com>
2020-05-26 20:16:46 -0400

    Implement function for repaying Aave loans

---
+++ contracts/aave-protocol/ILendingPool.sol
@@ -0,0 +1,30 @@
+pragma solidity ^0.6.8;
+
+interface ILendingPool {
+    function deposit(address _reserve, uint256 _amount, uint16 _referralCode) external;
+
+    function borrow(
+        address _reserve,
+        uint256 _amount,
+        uint256 _interestRateMode,
+        uint16 _referralCode
+    ) external;
+
+    function repay( address _reserve, uint256 _amount, address payable _onBehalfOf) external;
+
+    function getUserReserveData(address _reserve, address _user)
+        external
+        view
+        returns (
+            uint256 currentATokenBalance,
+            uint256 currentBorrowBalance,
+            uint256 principalBorrowBalance,
+            uint256 borrowRateMode,
+            uint256 borrowRate,
+            uint256 liquidityRate,
+            uint256 originationFee,
+            uint256 variableBorrowIndex,
+            uint256 lastUpdateTimestamp,
+            bool usageAsCollateralEnabled
+        );
+}
---
+++ contracts/aave-protocol/ILendingPoolAddressesProvider.sol
@@ -0,0 +1,6 @@
+pragma solidity ^0.6.8;
+
+interface ILendingPoolAddressesProvider {
+    function getLendingPool() external view returns (address);
+    function getLendingPoolCore() external view returns (address);
+}
