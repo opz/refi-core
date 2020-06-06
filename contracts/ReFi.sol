874660e96c9c85b01b3ae8aba34f832d5bbd4971
opz <will.shahda@gmail.com>
2020-06-05 20:09:51 -0400

    Implement function to set Uniswap router

--- contracts/ReFi.sol
+++ contracts/ReFi.sol
@@ -3,13 +3,18 @@ pragma solidity ^0.6.8;
 import {SafeMath} from "@openzeppelin/contracts/math/SafeMath.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 import {FixedPoint} from "@uniswap/lib/contracts/libraries/FixedPoint.sol";
+
+import {
+    IUniswapV2Router01
+} from "@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router01.sol";
 
 import {ILendingPoolAddressesProvider} from "./aave-protocol/ILendingPoolAddressesProvider.sol";
 import {ILendingPool} from "./aave-protocol/ILendingPool.sol";
 
-contract ReFi {
+contract ReFi is Ownable {
     //----------------------------------------
     // Type definitions
     //----------------------------------------
@@ -34,6 +39,8 @@ contract ReFi {
     // Cannot be declared constant because it is a non-value type
     FixedPoint.uq112x112 private _AAVE_REPAY_EPSILON; // solhint-disable-line var-name-mixedcase
 
+    IUniswapV2Router01 private _uniswapRouter;
+
     //----------------------------------------
     // Constructor
     //----------------------------------------
@@ -41,6 +48,18 @@ contract ReFi {
     constructor() public {
         // Set to 5%
         _AAVE_REPAY_EPSILON = FixedPoint.fraction(1, 20);
+    }
+
+    //----------------------------------------
+    // External functions
+    //----------------------------------------
+
+    /**
+     * @notice Set the Uniswap v2 router periphery contract
+     * @param uniswapRouter The router contract
+     */
+    function setUniswapRouter(IUniswapV2Router01 uniswapRouter) external onlyOwner {
+        _uniswapRouter = uniswapRouter;
     }
 
     //----------------------------------------
