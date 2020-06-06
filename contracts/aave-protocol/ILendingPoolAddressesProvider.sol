4603f878dcad4b9eec9488d655e684ca250ecc0f
opz <will.shahda@gmail.com>
2020-06-05 20:04:20 -0400

    Update Aave address provider interface for price oracle

--- contracts/aave-protocol/ILendingPoolAddressesProvider.sol
+++ contracts/aave-protocol/ILendingPoolAddressesProvider.sol
@@ -3,4 +3,5 @@ pragma solidity ^0.6.8;
 interface ILendingPoolAddressesProvider {
     function getLendingPool() external view returns (address);
     function getLendingPoolCore() external view returns (address);
+    function getPriceOracle() external view returns (address);
 }
